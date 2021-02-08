d45034c72c9896f788e67f0ca887bbbd1d7d70f4
Manuelpv17 <manuelpv17@outlook.com>
2021-02-07 21:22:54 -0500

    Add cron job to scraper

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+var CronJob = require("cron").CronJob;
+
+require("./database");
+
+const job = new CronJob("42 00 03 * * *", function () {
+  require("./hb_scraper");
+});
+job.start();
+
+app = express();
+
+// settings
+const PORT = process.env.PORT || 5001;
+
+// middelwars
+//app.use(cors());
+//app.use(express.json());
+
+//routes
+app.use("/api/v1/students", require("./routes/students"));
+app.use("/api/v1/projects", require("./routes/projects"));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
